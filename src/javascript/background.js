7376a2f7217a25a01f127b391998407d5d5abeb4
EunbinShin <38269665+EunbinShin@users.noreply.github.com>
2022-09-09 23:16:31 +0900

    기본 기능

---
+++ src/javascript/background.js
@@ -0,0 +1,7 @@
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = `src/image/${chosenImage}`;
+
+document.body.style.backgroundImage = `url(${bgImage})`
